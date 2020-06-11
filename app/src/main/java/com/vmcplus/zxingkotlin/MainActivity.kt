550f38d47c3a013c27a0400df9dce67495356c75
Riyas Valiyadan <riyas.vmc@gmail.com>
2020-06-11 21:23:18 +0530

    final

--- app/src/main/java/com/vmcplus/zxingkotlin/MainActivity.kt
+++ app/src/main/java/com/vmcplus/zxingkotlin/MainActivity.kt
@@ -5,13 +5,8 @@ import android.graphics.Color
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.zxing.BarcodeFormat
-import com.google.zxing.EncodeHintType
-import com.google.zxing.Writer
-import com.google.zxing.WriterException
+import com.google.zxing.*
 import com.google.zxing.common.BitMatrix
-import com.google.zxing.oned.Code128Writer
-import com.google.zxing.qrcode.QRCodeWriter
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
@@ -22,43 +17,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Generating Barcode
-        val bitmap = generateBarCode("9037495473")
+        // Generating QR-Code
+        val bitmap = generateAnyCode("9037495473", BarcodeFormat.QR_CODE)
 
-        // Generating QR Code
-        //val bitmap = generateQRCode("9037495473")
+        // Generating Barcode
+        //val bitmap = generateAnyCode("9037495473", BarcodeFormat.CODE_128)
 
         imageView.setImageBitmap(bitmap)
 
     }
 
-    private fun generateQRCode(string: String): Bitmap {
-        val width = 500
-        val height = 500
-        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
-        val codeWriter: Writer = QRCodeWriter()
-        try {
-            val bitMatrix: BitMatrix = codeWriter.encode(string, BarcodeFormat.QR_CODE, width, height)
-            for (x in 0 until width) {
-                for (y in 0 until height) {
-                    bitmap.setPixel(x, y, if (bitMatrix[x, y]) Color.BLACK else Color.WHITE)
-                }
-            }
-        } catch (e: WriterException) {
-            Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()
-        }
-        return bitmap
-    }
-
-    private fun generateBarCode(string: String): Bitmap {
+    private fun generateAnyCode(string: String, codeFormat: BarcodeFormat): Bitmap {
         val width = 400
-        val height = 200
+        val height = 400
         val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
-        val codeWriter: Writer = Code128Writer()
+        val codeWriter: Writer = MultiFormatWriter()
         try {
             val hintMap: Hashtable<EncodeHintType, ErrorCorrectionLevel> = Hashtable<EncodeHintType, ErrorCorrectionLevel>()
             hintMap[EncodeHintType.ERROR_CORRECTION] = ErrorCorrectionLevel.L
-            val byteMatrix: BitMatrix = codeWriter.encode(string, BarcodeFormat.CODE_128, width, height, hintMap)
+            val byteMatrix: BitMatrix = codeWriter.encode(string, codeFormat, width, height, hintMap)
             for (x in 0 until width) {
                 for (y in 0 until height) {
                     bitmap.setPixel(x, y, if (byteMatrix[x, y]) Color.BLACK else Color.WHITE)
